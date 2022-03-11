5f020a4266b6578e5e634d5f5b510986797c757d
songyh119 <yh.song@boozaapp.com>
2022-03-11 11:46:35 +0900

    feat: firebase app

---
+++ src/firebase.tsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCQZ6xSyd9g3bqCI0eUiDOkMpFPL3ynPbI",
+  authDomain: "twitter-clone-48aec.firebaseapp.com",
+  projectId: "twitter-clone-48aec",
+  storageBucket: "twitter-clone-48aec.appspot.com",
+  messagingSenderId: "234716993861",
+  appId: "1:234716993861:web:7ab6462465bcd9def1062c",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
