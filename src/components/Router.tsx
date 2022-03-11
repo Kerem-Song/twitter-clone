5b05828d1330a2c5b5c2e56e9dc871ed4265c5fc
songyh119 <yh.song@boozaapp.com>
2022-03-11 16:19:49 +0900

    feat: router

---
+++ src/components/Router.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
+
+const AppRouter = () => {
+  const [isLoggedIn, sestIsLoggedIn] = useState(false);
+
+  return (
+    <Router>
+      <Routes>
+        {isLoggedIn ? (
+          <>
+            <Route path="/" element={<Home />} />
+          </>
+        ) : (
+          <>
+            <Route path="/" element={<Auth />} />
+          </>
+        )}
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
