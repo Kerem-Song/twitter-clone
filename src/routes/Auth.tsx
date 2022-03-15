f77e99ad5c1d3522a06d8173a33685f5de233023
songyh119 <yh.song@boozaapp.com>
2022-03-15 17:38:21 +0900

    feat: login form frame

--- src/routes/Auth.tsx
+++ src/routes/Auth.tsx
@@ -1,5 +1,49 @@
-import React from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = (e: FormEvent) => {
+    console.log(e);
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          value={email}
+          required
+          onChange={(e) => onChange(e)}
+        />
+        <input
+          name="password"
+          type="text"
+          placeholder="Password"
+          value={password}
+          required
+          onChange={(e) => onChange(e)}
+        />
+        <input type="submit" value="Log In" />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
+};
 
 export default Auth;
