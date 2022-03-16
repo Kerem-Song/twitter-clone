9980bbefbdf1b244ee6031bfe57e4816325d5ece
songyh119 <yh.song@boozaapp.com>
2022-03-16 17:31:19 +0900

    feat: log out and route for redirection

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,20 +1,31 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Navigation from "components/Navigation";
+import Profile from "routes/Profile";
 import { IsLoggedIn } from "../App";
 
 const AppRouter = ({ isLoggedIn }: IsLoggedIn) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
           <>
             <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="*" element={<Navigate to="/" />} />
           </>
         ) : (
           <>
             <Route path="/" element={<Auth />} />
+            <Route path="*" element={<Navigate to="/" />} />
           </>
         )}
       </Routes>
--- src/routes/Profile.tsx
+++ src/routes/Profile.tsx
@@ -1,5 +1,26 @@
 import React from "react";
+import { authService } from "fb";
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
-const Profile = () => <span>Profile</span>;
+const Profile = () => {
+  const navigate = useNavigate();
+
+  const handleSignOut = () => {
+    signOut(authService)
+      .then(() => {
+        console.log("logout success");
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log("log out failed: ", err);
+      });
+  };
+  return (
+    <>
+      <button onClick={handleSignOut}>Log Out</button>
+    </>
+  );
+};
 
 export default Profile;
