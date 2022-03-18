2ce211a0062ab92f53edd6527bc109a964e7a8a0
songyh119 <yh.song@boozaapp.com>
2022-03-18 11:45:59 +0900

    feat: firestore and database for tweet

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,42 @@
-import React from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import { dbService } from "fb";
+import { collection, addDoc } from "firebase/firestore";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [tweet, setTweet] = useState("");
+
+  const onChange = ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
+    setTweet(value);
+  };
+  const onSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      const docRef = await addDoc(collection(dbService, "tweets"), {
+        tweet,
+        createdAt: Date.now(),
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+
+    setTweet("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="what's on your mind?"
+          maxLength={120}
+          value={tweet}
+          onChange={onChange}
+        />
+        <input type="submit" value="tweet" />
+      </form>
+    </div>
+  );
+};
 
 export default Home;
